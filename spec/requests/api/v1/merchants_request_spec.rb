b00bef7656fa3cd5b4369179a39bee903d51797f
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-16 17:22:57 -0500

    update merchant spec with updated sequalizer information

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,5 +8,16 @@ describe "Merchants API" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a (String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a (String)
+    end
   end
 end
