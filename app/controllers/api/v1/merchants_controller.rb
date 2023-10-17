8daede128afa6b8a7563a6e9ba3e7f937a126115
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-16 21:49:10 -0500

    update of merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,7 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.all)
   end
 
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
 end
