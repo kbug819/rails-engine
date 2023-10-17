0c7fdada45165daa36b318f8b1277b95e7114a83
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-16 21:52:12 -0500

    testing for merchant spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -16,8 +16,63 @@ describe "Merchants API" do
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_a (String)
 
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to eq("merchant")
+
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a (String)
+    end
+  end
+
+  it "sends a list of one specific merchant" do
+    
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a (String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a (String)
+  end
+
+  it "should return all items associated with a merchant" do
+    merchant = create(:merchant)
+    create(:item, merchant: merchant)
+    create(:item, merchant: merchant)
+    create(:item, merchant: merchant)
+    id = merchant.id
+
+    get "/api/v1/merchants/#{id}/items"
+    # get api_v1_
+    expect(response).to be_successful
+
+    merchant_items = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(merchant_items.count).to eq(3)
+    merchant_items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a (String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq("item")
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a (String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a (String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a (Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a (Integer)
+      expect(item[:attributes][:merchant_id]).to eq(id)
     end
   end
 end
