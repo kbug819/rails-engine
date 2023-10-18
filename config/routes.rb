3e11237a19af43e036bcfadd2785786f2cad5500
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-18 12:19:51 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,22 @@ Rails.application.routes.draw do
   # get "/api/v1/merchants/:id/items", to: "api/v1/merchants/items"
   # get "/api/v1/merchants/:merchant_id/items", to: "api/v1/merchants/items"
 
+  get "/api/v1/items/find_all", to: "api/v1/items/search#index"
+  get "/api/v1/items/find", to: "api/v1/items/search#show"
+
+  get "/api/v1/merchants/find_all", to: "api/v1/merchants/search#index"
+  get "/api/v1/merchants/find", to: "api/v1/merchants/search#show"
+
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        get "/items", to: 'merchants/items#index'
+        resources :items, controller: "merchants/items"
+        # resources :find, controller: "merchants/search", as: :merchant_search
       end
       resources :items do
-        get "/merchant", to: "items/merchant#index"
+        resources :merchant, controller: "items/merchant"
+        # resources :find, controller: "items/search", only: [:index, :show]
+
       end
     end
   end
