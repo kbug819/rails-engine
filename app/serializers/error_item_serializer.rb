2978cd573dbdb69766a9c5a7af03b13d82f69d0f
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-19 11:54:10 -0500

    update of error serializer

--- app/serializers/error_item_serializer.rb
+++ app/serializers/error_item_serializer.rb
@@ -14,4 +14,26 @@ class ErrorItemSerializer
       ]
     }
   end
+
+  def serialized_json_search
+    if @error == "data"
+    {
+      data: {}
+    }
+    end
+  end
+
+  def serialized_json_min_max
+    if @error == "error"
+      {
+        errors: [
+          {
+            status: "Cannot find by less than 0",
+            message: "cannot find by less than 0",
+            code: 400
+          }
+        ]
+      }
+    end
+  end
 end
