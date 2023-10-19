d9e62e3bc9fe85657b04644e03ef9cda4e7d02e4
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-19 15:30:56 -0500

    facade update for sad paths

--- app/facades/item_facade.rb
+++ app/facades/item_facade.rb
@@ -10,4 +10,13 @@ class ItemFacade
       result = ErrorItem.new("No merchants found", 'Not Found', 404)
     end
   end
+
+  def find_item_for_merchant
+    if Item.pluck(:id).include?(@params[:item_id].to_i)
+      item = Item.find(@params[:item_id])
+    else
+      result = ErrorItem.new("No items found", 'Not Found', 404)
+    end
+  end
+
 end
