1ee6bef1ecb1a29f39cb34a441ed21a30ee287c9
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-19 15:31:53 -0500

    error serializer updated for sad paths

--- app/serializers/error_item_serializer.rb
+++ app/serializers/error_item_serializer.rb
@@ -28,8 +28,8 @@ class ErrorItemSerializer
       {
         errors: [
           {
-            status: "Cannot find by less than 0",
-            message: "cannot find by less than 0",
+            status: "Cannot Find",
+            message: "Either min/max is below 0, or you cannot pass name and price params together",
             code: 400
           }
         ]
