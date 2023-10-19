584a8ad2eac7f614f9dbd5dbff30c86cd3e54da3
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-19 11:52:51 -0500

    update of search controller for items

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,17 +1,20 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
     items = Item.find_all_items(params)
-    # if items = []
-    #   render json: ItemSerializer.new(items), status: 400
-    # else
+    if items == []
+      render json: ItemSerializer.new(items), status: 400
+    elsif items == "error"
+      render json: ErrorItemSerializer.new(items).serialized_json_min_max, status: 400
+    else
       render json: ItemSerializer.new(items)
-    # end
+    end
   end
   def show
-    if Item.find_one_item(params[:name])[0] != nil
-      render json: ItemSerializer.new(Item.find_one_item(params[:name])[0])
+    if Item.find_one_item(params[:name])[0] == nil
+      render json: ErrorItemSerializer.new("data").serialized_json_search
     else 
-      render json: ErrorItemSerializer.new(ErrorItem.new("No items found", 'Not Found', 404)).serialized_json
+      render json: ItemSerializer.new(Item.find_one_item(params[:name])[0])
+      # render json: ErrorItemSerializer.new(ErrorItem.new("No items found", 'Not Found', 404)).serialized_json
     end
 
   end
