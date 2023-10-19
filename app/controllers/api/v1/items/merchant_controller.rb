ebf2ae916962af2d004697df1882d9d1170d7a3c
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-19 15:29:07 -0500

    sad path updates for merchant controller

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Items::MerchantController < ApplicationController
   def index
-    item = Item.find(params[:item_id])
-    render json: MerchantSerializer.new(item.merchant)
+    @item = ItemFacade.new(params).find_item_for_merchant
+    if @item.class == Item
+      render json: MerchantSerializer.new(@item.merchant)
+    else
+      render json: ErrorItemSerializer.new(@item).serialized_json, status: 404
+    end
   end
 end
