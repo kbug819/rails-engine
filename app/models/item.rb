c1d2b19059dcaebabf10bdef18bbc46c562eef61
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-19 17:10:05 -0500

    model update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,5 @@
 class Item < ApplicationRecord
-
   belongs_to :merchant
-
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
@@ -10,31 +8,47 @@ class Item < ApplicationRecord
     Item.where('lower(name) LIKE lower(?)', "%#{query}%").order(:name).limit(1)
   end
 
+  def self.name_present(params)
+    Item.where('lower(name) LIKE lower(?)', "%#{params["name"]}%").order(:name)
+  end
+
+  def self.min_and_max_present(params)
+    Item.where('unit_price >= ? AND unit_price <= ?', "#{params["min_price"]}", "#{params["max_price"]}" ).order(unit_price: :desc)
+  end
+
+  def self.min_present(params)
+    if params["min_price"].to_i < 0
+      items = "error"
+    else
+      Item.where('unit_price >= ?', "#{params["min_price"]}").order(unit_price: :desc)
+    end
+  end
+
+  def self.max_present(params)
+    if params["max_price"].to_i < 0
+      items = "error"
+    else
+      items = Item.where('unit_price <= ?', "#{params["max_price"]}").order(unit_price: :desc)
+    end
+  end
+
   def self.find_all_items(params)
     if params["name"] && params["min_price"].present?
-      items = "error"
+      "error"
     elsif params["name"] && params["max_price"].present?
       items = "error"
     elsif params["name"].present?
-      items = Item.where('lower(name) LIKE lower(?)', "%#{params["name"]}%").order(:name)
+      self.name_present(params)
     elsif params["min_price"] && params["max_price"].present?
-      items = Item.where('unit_price >= ? AND unit_price <= ?', "#{params["min_price"]}", "#{params["max_price"]}" ).order(unit_price: :desc)
+      self.min_and_max_present(params)
     elsif params["min_price"].present?
-      if params["min_price"].to_i < 0
-        items = "error"
-      else
-        items = Item.where('unit_price >= ?', "#{params["min_price"]}").order(unit_price: :desc)
-      end
+      self.min_present(params)
     elsif params["max_price"].present?
-      if params["max_price"].to_i < 0
-        items = "error"
-      else
-        items = Item.where('unit_price <= ?', "#{params["max_price"]}").order(unit_price: :desc)
-      end
+      self.max_present(params)
     else
-      items = []
+      []
     end
   end
+
 end
 
-# Item.where('lower(name) LIKE lower(?)', "%kaylee%").order(:name)
